a8c12ff401f2da7dd68b7b0c804d6b73a7539b91
nnamchisimi <mosimiloluwannamchi@gmail.com>
2024-07-24 16:50:17 +0300

    updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the Excel file into a global DataFrame
-file_path = r'C:\Users\User\Desktop\web\gniv_StarOrderplus_Order_items.xlsx'  # Replace with your file path
+file_path = r'C:\Users\User\Desktop\web\updated_StarOrderplus_Order_items (1).xlsx'  # Replace with your file path
 df = pd.read_excel(file_path)
 
 # List of columns to display
@@ -22,7 +22,9 @@ columns_to_display = [
     'Q ordered',
     'Customer info',
     'Part number',
+    'Tracking No',
     'StockEntryDate'
+    
 ]
 
 @app.route('/', methods=['GET', 'POST'])
