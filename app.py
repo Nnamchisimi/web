d6b7ba06aa7bd01fa027c199206fda10f93ca915
nnamchisimi <mosimiloluwannamchi@gmail.com>
2024-07-27 10:57:47 +0300

    added tracking details link

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the Excel file into a global DataFrame
-file_path = r'C:\Users\User\Desktop\web\dupdated_StarOrderplus_Order_items_with_status.xlsx'  # Replace with your file path
+file_path = r'C:\Users\User\Desktop\web\aupdated_StarOrderplus_Order_items_with_expected_stock_entry_date (1).xlsx'  # Replace with your file path
 df = pd.read_excel(file_path)
 
 # List of columns to display
 columns_to_display = [
-    
     'Designation of part no. ordered',
     'Designation of part no. confirmed',
     'Ord. date',
@@ -22,11 +21,12 @@ columns_to_display = [
     'Q ordered',
     'Customer info',
     'Part number',
+    'Dist. ch.',
     'Tracking No',
+    'tracking_details',
     'expectedstockentrydate',
     'status',
     'StockEntryDate'
-    
 ]
 
 @app.route('/', methods=['GET', 'POST'])
@@ -37,10 +37,16 @@ def index():
         if 'Customer info' in df.columns:
             part_details = df[df['Customer info'].astype(str).str.contains(pn_ordered, case=False, na=False)]
             part_details = part_details[columns_to_display]  # Filter columns
-        else:
-            part_details = pd.DataFrame(columns=columns_to_display)  # Empty DataFrame with specified columns if column not found
 
-    return render_template('index.html', part_details=part_details)
+            # Convert tracking_details to clickable links
+            part_details['tracking_details'] = part_details['tracking_details'].apply(lambda x: f'<a href="{x}" target="_blank">{x}</a>' if pd.notnull(x) and x != 'Not Found' else '')
+
+            # Convert DataFrame to HTML and allow for safe rendering
+            part_details_html = part_details.to_html(classes='table table-striped', escape=False, index=False)
+        else:
+            part_details_html = '<p>Column "Customer info" does not exist in the Excel file.</p>'
+
+    return render_template('index.html', part_details=part_details_html)
 
 if __name__ == '__main__':
     app.run(debug=True)
